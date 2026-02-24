1a5ca34676d9b11dfd7e1fdd525292bffa8e4a2e
atv 7911 <atv7911@gmail.com>
2026-02-24 07:55:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-362353104-7338e",
+  "appId": "1:705266653377:web:30dd70ed9e0dade1b0dc40",
+  "apiKey": "AIzaSyAPuyx0Qy-kUSmE7Pv3F0BnHlW1vhN-S7U",
+  "authDomain": "studio-362353104-7338e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "705266653377"
+};
