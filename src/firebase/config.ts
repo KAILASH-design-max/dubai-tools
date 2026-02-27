b6eb44334c12df0e08aa237ad6ddb366d6d40548
atv 7911 <atv7911@gmail.com>
2026-02-27 16:31:45 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyChM71XdlOjWg6sYAJhqh0z8vXG2RHX3DY",
-  authDomain: "ranil-physio-digital.firebaseapp.com",
-  databaseURL: "https://ranil-physio-digital-default-rtdb.firebaseio.com",
-  projectId: "ranil-physio-digital",
-  storageBucket: "ranil-physio-digital.firebasestorage.app",
-  messagingSenderId: "766656526047",
-  appId: "1:766656526047:web:3764c4dc80493e36669b6a",
-  measurementId: "G-5JBSWGESZY"
+  apiKey: "AIzaSyCkIocYy-GS4NWt0l8ov1ISH5FWEUoeVXw",
+  authDomain: "studio-516008895-96df1.firebaseapp.com",
+  projectId: "studio-516008895-96df1",
+  storageBucket: "studio-516008895-96df1.firebasestorage.app",
+  messagingSenderId: "681213034456",
+  appId: "1:681213034456:web:9717b4ad2cb793b19b3cad"
 };
