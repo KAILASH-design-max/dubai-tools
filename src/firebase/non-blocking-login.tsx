a90e0756a817c42b2e5c8c318c7b4b30529f6892
atv 7911 <atv7911@gmail.com>
2026-03-01 05:55:51 +0000

    add the direct login and sign up with goggle

--- src/firebase/non-blocking-login.tsx
+++ src/firebase/non-blocking-login.tsx
@@ -1,9 +1,12 @@
+
 'use client';
 import {
   Auth,
   signInAnonymously,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
 } from 'firebase/auth';
 
 /** Initiate anonymous sign-in (non-blocking). */
@@ -19,4 +22,10 @@ export function initiateEmailSignUp(authInstance: Auth, email: string, password:
 /** Initiate email/password sign-in (non-blocking). */
 export function initiateEmailSignIn(authInstance: Auth, email: string, password: string) {
   return signInWithEmailAndPassword(authInstance, email, password);
+}
+
+/** Initiate Google sign-in (non-blocking). */
+export function initiateGoogleSignIn(authInstance: Auth) {
+  const provider = new GoogleAuthProvider();
+  return signInWithPopup(authInstance, provider);
 }
